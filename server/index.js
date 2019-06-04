e88f1fce8453701204bb88250a2f21bbf6251e4c
Joshua Herrera <herrjosh@oregonstate.edu>
2019-06-04 16:26:49 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express(); //generates running express app
+
+app.get('/', (req, res) => {
+	res.send({ hi: 'there'});
+});
+
+const PORT = process.env.PORT || 5000; //heroku can inject the env var 
+app.listen(PORT); //tells node to listen on this port
+//localhost:5000
