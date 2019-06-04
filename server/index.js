9d486db23eb4b9408d09510d684db101c045071d
Joshua Herrera <herrjosh@oregonstate.edu>
2019-06-04 16:48:26 -0700

    changed greeting

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); //generates running express app
 
 app.get('/', (req, res) => {
-	res.send({ hi: 'there'});
+	res.send({ sup: 'cuh'});
 });
 
 const PORT = process.env.PORT || 5000; //heroku can inject the env var 
