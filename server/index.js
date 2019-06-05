b6ce314cd5d7f310bf2fcdda4c5e1950a05dbc3e
Joshua Herrera <herrjosh@oregonstate.edu>
2019-06-04 21:50:47 -0700

    added keys file for dev

--- server/index.js
+++ server/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express(); //generates running express app
 
-app.get('/', (req, res) => {
-	res.send({ sup: 'cuh'});
-});
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		(accessToken) => {
+			console.log(accessToken);
+		}
+	)
+); //be able to auth with google
 
-const PORT = process.env.PORT || 5000; //heroku can inject the env var 
+app.get(
+	'/auth/google', //use passport's request method; GoogleStrategy has internal ID of 'google'
+	passport.authenticate('google', {
+		scope: ['profile', 'email'] //specifies what info to access from google, its profile and email. names controlled by google
+	})
+);
+
+const PORT = process.env.PORT || 5000; //heroku can inject the env var
 app.listen(PORT); //tells node to listen on this port
 //localhost:5000
